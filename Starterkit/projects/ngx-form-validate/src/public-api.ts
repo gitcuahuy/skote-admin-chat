f9af20812e0ee148ceb8d0b8d524f14887ead67b
Huy nq <huy.nguyenquang@evotek.vn>
2023-06-04 15:39:08 +0700

    init auth

--- Starterkit/projects/ngx-form-validate/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngx-form-validate
- */
-
-export * from './lib/ngx-form-validate.service';
-export * from './lib/ngx-form-validate.component';
-export * from './lib/ngx-form-validate.module';
