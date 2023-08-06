4b85e4eb7ce850554cbc9c836460a845d328e02f
Huy nq <huy.nguyenquang@evotek.vn>
2023-08-06 22:18:20 +0700

    init auth

--- Starterkit/src/app/core/models/auth.models.ts
+++ Starterkit/src/app/core/models/auth.models.ts
@@ -47,6 +47,7 @@ export class User implements  IUser{
   lastLoginAt?: string;
   lastAuthChangeAt?: string;
   userPrimary?: IUserPrimary;
+  departmentId?: string;
   departmentName?: string;
   latitude?: number;
   longitude?: number;
@@ -58,7 +59,8 @@ export class User implements  IUser{
   fullName?: string;
   username?: string;
   role?: IRole;
-  email?: string;
+  email?: string; // email công ty
+  personal_email?: string; // email cá nhân
   emailVerified?: boolean;
   phoneNumber?: string;
   title?: string;
@@ -67,6 +69,17 @@ export class User implements  IUser{
   address?: string;
   description?: string;
   status?: UserStatus;
+  identifyCardNumber?: string; // số căn cước coond dân
+  provide?: string; // nơi cấp
+  supply_date?: Date; // ngày cấp
+  shelter?: string; // nơi ở hiện tại
+  placeOfResidence?: string; // nơi thường trú
+  taxCode?: string; // mã số thuế
+  education?: string; // trình độ học vấn
+  school?: string; // trường học
+  major?: string; // chuyên ngành
+  graduationYear?: Date; // năm tốt nghiệp
+  maritalStatus?: string; // tình trạng hôn nhân
 
   search_fullName?: string;
   search_partials?: string[];
--- Starterkit/src/app/pages/employees/employee-list/employee-update/employee-update.component.ts
+++ Starterkit/src/app/pages/employees/employee-list/employee-update/employee-update.component.ts
@@ -8,13 +8,27 @@ export const USER_FORM_FIELDS = {
   fullName: 'fullName',
   username: 'username',
   email: 'email',
+  personal_email: 'personal_email', // email cá nhân
   phoneNumber: 'phoneNumber',
   dayOfBirth: 'dayOfBirth',
+  avatarUrl: 'avatarUrl',
   address: 'address',
   description: 'description',
   status: 'status',
   roleId: 'roleId',
   departmentId: 'departmentId',
+  gender: 'gender',
+  identifyCardNumber: 'identifyCardNumber',
+  provide: 'provide',
+  supply_date: 'supply_date',
+  shelter: 'shelter',
+  placeOfResidence: 'placeOfResidence',
+  taxCode: 'taxCode',
+  education: 'education',
+  school: 'school',
+  major: 'major',
+  graduationYear: 'graduationYear',
+  maritalStatus: 'maritalStatus',
 }
 
 @Component({
@@ -25,8 +39,9 @@ export const USER_FORM_FIELDS = {
 export class EmployeeUpdateComponent implements OnInit {
   readonly breadCrumbItems = [{label: 'Quản lý nhân sự'}, {label: 'danh sách nhân sự', active: true}];
   readonly avatarPlaceholder = 'assets/images/users/Profile_avatar_placeholder_large.png';
+  readonly USER_FORM_FIELDS = USER_FORM_FIELDS;
   avatar: NzUploadFile[] = [];
-  domainData: User[] = [];
+  domainData?: User;
   formHandler?: FormGroup;
 
   constructor(private fb: FormBuilder,
@@ -34,11 +49,35 @@ export class EmployeeUpdateComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.initForm(this.domainData)
   }
 
   initForm(data?: User): void {
     this.formHandler = this.fb.group({
-
+      [USER_FORM_FIELDS.fullName]: [data?.fullName],
+      [USER_FORM_FIELDS.username]: [data?.username],
+      [USER_FORM_FIELDS.email]: [data?.email],
+      [USER_FORM_FIELDS.personal_email]: [data?.personal_email],
+      [USER_FORM_FIELDS.phoneNumber]: [data?.phoneNumber],
+      [USER_FORM_FIELDS.dayOfBirth]: [data?.dayOfBirth],
+      [USER_FORM_FIELDS.address]: [data?.address],
+      [USER_FORM_FIELDS.description]: [data?.description],
+      [USER_FORM_FIELDS.status]: [data?.status],
+      [USER_FORM_FIELDS.roleId]: [data?.role?.id],
+      [USER_FORM_FIELDS.departmentId]: [data?.departmentId],
+      [USER_FORM_FIELDS.gender]: [data?.gender],
+      [USER_FORM_FIELDS.avatarUrl]: [data?.avatarUrl],
+      [USER_FORM_FIELDS.identifyCardNumber]: [data?.identifyCardNumber],
+      [USER_FORM_FIELDS.provide]: [data?.provide],
+      [USER_FORM_FIELDS.supply_date]: [data?.supply_date],
+      [USER_FORM_FIELDS.shelter]: [data?.shelter],
+      [USER_FORM_FIELDS.placeOfResidence]: [data?.placeOfResidence],
+      [USER_FORM_FIELDS.taxCode]: [data?.taxCode],
+      [USER_FORM_FIELDS.education]: [data?.education],
+      [USER_FORM_FIELDS.school]: [data?.school],
+      [USER_FORM_FIELDS.major]: [data?.major],
+      [USER_FORM_FIELDS.graduationYear]: [data?.graduationYear],
+      [USER_FORM_FIELDS.maritalStatus]: [data?.maritalStatus],
     })
   }
 
